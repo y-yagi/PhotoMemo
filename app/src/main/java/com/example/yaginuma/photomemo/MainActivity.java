6f1ff8559a06e12f0e430e8796994c9b8b3bd6b3
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2015-09-21 10:10:15 +0900

    organize menu

--- app/src/main/java/com/example/yaginuma/photomemo/MainActivity.java
+++ app/src/main/java/com/example/yaginuma/photomemo/MainActivity.java
@@ -80,17 +80,11 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camara) {
+        if (id == R.id.nav_map) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
         } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
 
         }
 
