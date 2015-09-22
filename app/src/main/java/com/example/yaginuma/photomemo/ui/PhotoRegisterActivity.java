43406ce3662f9db49b4b1fad90a5fb87d618d0a5
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2015-09-23 07:58:00 +0900

    remove useless import

--- app/src/main/java/com/example/yaginuma/photomemo/ui/PhotoRegisterActivity.java
+++ app/src/main/java/com/example/yaginuma/photomemo/ui/PhotoRegisterActivity.java
@@ -4,39 +4,19 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.content.Intent;
-import android.media.ExifInterface;
 import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
 import android.net.Uri;
-import android.os.AsyncTask;
-
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.text.TextUtils;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.example.yaginuma.photomemo.R;
-
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
 import models.Photo;
@@ -71,7 +51,7 @@ public class PhotoRegisterActivity extends AppCompatActivity {
 
         if (Intent.ACTION_SEND.equals(action) && type != null) {
             if (type.startsWith("image/")) {
-                handleSendImage(intent); // Handle single image being sent
+                handleSendImage(intent);
             }
         }
 
